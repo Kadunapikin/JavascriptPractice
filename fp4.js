d96d1e9dece99ecb439b0057dd22e580d5223009
kadunapikin <ibakoshi27@gmail.com>
2023-01-26 18:54:20 +0100

    write a code to access different items from cars and rides array

--- fp4.js
+++ fp4.js
@@ -6,3 +6,8 @@ document.getElementById("demo1").innerHTML = cars
 //Using the JavaScript Keyword new
 const rides = new Array ("Saab", "Volvo", "BMW");
 document.getElementById("demo2").innerHTML =rides
+
+//Accessing Array Elements
+document.getElementById("demo3").innerHTML = cars[0]
+document.getElementById("demo4").innerHTML =rides[2]
+
