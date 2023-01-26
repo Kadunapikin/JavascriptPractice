fb6a1d4c6a1e943df036aa2e27f520a3a7d2a392
kadunapikin <ibakoshi27@gmail.com>
2023-01-26 18:40:02 +0100

    write a code to create rides array using js keyword new and display the array items on the webpage

--- fp4.js
+++ fp4.js
@@ -1,6 +1,8 @@
 //Creating and Array
 
 const cars = ["Saab", "Volvo", "BMW"];
-
 document.getElementById("demo1").innerHTML = cars
 
+//Using the JavaScript Keyword new
+const rides = new Array ("Saab", "Volvo", "BMW");
+document.getElementById("demo2").innerHTML =rides
